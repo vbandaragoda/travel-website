b8fc886226f251634c7ec7e8b72bde210169e616
Vimukthi Nelanga <vimukthi.nelanga@gmail.com>
2021-08-21 20:47:23 +0530

    Second one

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,55 @@
-import React from 'react'
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
 
 function Cards() {
     return (
-        <div className="cards">
+        <div className='cards'>
             <h1>Check out these EPIC Destinations!</h1>
-            <div className="cards__container"></div>
-            <div className="cards__wrapper"></div> 
-            
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src={require('../images/img-9.jpg').default}
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src={require('../images/img-2.jpg').default}
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src={require('../images/img-9.jpg').default}
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src={require('../images/img-2.jpg').default}
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src={require('../images/img-2.jpg').default}
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+
+
+                    </ul>
+
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Cards
+export default Cards;
