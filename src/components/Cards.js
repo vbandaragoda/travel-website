af0b705cfb1ab5cc9e85c20342f3b6fcd09dc429
Vimukthi Nelanga <vimukthi.nelanga@gmail.com>
2021-08-22 20:41:14 +0530

    Last 1

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,53 +3,49 @@ import './Cards.css';
 import CardItem from './CardItem';
 
 function Cards() {
-    return (
-        <div className='cards'>
-            <h1>Check out these EPIC Destinations!</h1>
-            <div className='cards__container'>
-                <div className='cards__wrapper'>
-                    <ul className='cards__items'>
-                        <CardItem
-                            src={require('../images/img-9.jpg').default}
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
-                            path='/services'
-                        />
-                        <CardItem
-                            src={require('../images/img-2.jpg').default}
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
-                            path='/services'
-                        />
-
-                    </ul>
-                    <ul className='cards__items'>
-                        <CardItem
-                            src={require('../images/img-9.jpg').default}
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
-                            path='/services'
-                        />
-                        <CardItem
-                            src={require('../images/img-2.jpg').default}
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
-                            path='/services'
-                        />
-                        <CardItem
-                            src={require('../images/img-3.jpg').default}
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
-                            path='/services'
-                        />
-
-
-                    </ul>
-
-                </div>
-            </div>
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src={require('../images/img-9.jpg').default}
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src={require('../images/img-2.jpg').default}
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/products'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src={require('../images/img-3.jpg').default}
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/products'
+            />
+            <CardItem
+              src={require('../images/img-4.jpg').default}
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src={require('../images/img-8.jpg').default}
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/products'
+            />
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
